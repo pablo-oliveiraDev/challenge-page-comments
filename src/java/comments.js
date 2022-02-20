806c5f347fc0274cf6223b0d186d9c61306eac50
pablo oliveira <pabloliverfe@gmail.com>
2022-02-20 15:27:04 -0300

    terminado backennd

---
+++ src/java/comments.js
@@ -0,0 +1,45 @@
+function render(dados) {
+    var html = "<div class='comentbox'><div class='left-painel-img '><img src='https://via.placeholder.com/80x80.png?'class=' rounded-circle 'alt='80x80'  data-holder-rendered='true'></div><div class='right-painel'><span>" + dados.name  + "</span><div class='date'>" + dados.data + "</div><p>" + dados.body + "</p></div><div class='clear clearfix'></div></div>";
+    $('#text-menssage').append(html)
+
+}
+$(document).ready(function () {
+    var comment=[];
+        
+    if(!localStorage.commentsData){
+        localStorage.commentsData = [];
+    }else{
+        comment=JSON.parse(localStorage.commentsData);
+    }
+    
+    for (var i = 0; i < comment.length; i++) {
+        render(comment[i]);
+        
+
+    }
+    $('#addcomment').click(function () {
+        function converdata(){
+            const teste=$('#data').val().split('-')
+        var dd=teste[2],mm=teste[1],yyyy=teste[0]
+        return dd+'/'+mm+'/'+yyyy;
+        }
+        
+        
+        var addObj = {
+            name: $('#name').val(),
+            data: converdata(),
+            body: $('#bodytext').val()
+        };
+        // localStorage.commentsData=JSON.stringify(comment)
+        comment.push(addObj);
+        localStorage.setItem('commentsData',JSON.stringify(comment));
+        render(addObj);
+        $('#name').val('');
+        $('#data').val('');
+        $('#bodytext').val('');
+
+    });
+
+});
+
+
